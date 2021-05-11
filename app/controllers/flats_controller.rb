f2523b142323594c3f5c206880973cdb7e6c7ea0
Allison Sessé <allisonsesse@gmail.com>
2021-05-11 11:47:50 +0200

    update, destroy

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,10 @@
 class FlatsController < ApplicationController
-
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
     @flats = Flat.all
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -17,16 +16,28 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
-    @flat.save
+    if @flat.save
+      redirect_to flat_path(@flat), notice: "#{@flat.name} has been successfully created"
+    else
+      render :new
+    end
   end
 
   def update
+    @flat.update(flat_params)
+    redirect_to flats_path
   end
 
   def destroy
+    @flat.destroy
+    redirect_to flats_path, notice: "#{@flat.name} has been successfully deleted"
   end
 
   private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
     params.require(:flat).permit(:name, :address, :price_per_night, :number_of_guests)
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,7 +1,6 @@
 class Flat < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
-  validates :description, presence: true
   validates :price_per_night, presence: true
   validates :number_of_guests, presence: true
 end
